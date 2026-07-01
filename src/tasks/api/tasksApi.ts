e3878e29ce14c1bb0d2c655873d9b949bb46e329
Pecyn <pecynw@gmail.com>
2026-07-01 16:51:49 +0200

    feat: add getTasks endpoint and TaskList component with loading/error states

--- src/tasks/api/tasksApi.ts
+++ src/tasks/api/tasksApi.ts
@@ -1,7 +1,14 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import type { Task } from '../types'
 
 export const tasksApi = createApi({
   reducerPath: 'tasksApi',
   baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_API_BASE_URL }),
-  endpoints: () => ({}),
+  endpoints: (builder) => ({
+    getTasks: builder.query<Task[], void>({
+      query: () => '/tasks',
+    }),
+  }),
 })
+
+export const { useGetTasksQuery } = tasksApi
